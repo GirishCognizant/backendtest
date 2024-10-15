b4dec08a46c6db956a9c115c2eb0321a8e67ffa3
GirishCognizant <Girish.KumarSP@cognizant.com>
2024-10-15 11:35:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const blogRouter = require("./route/blog-route");
 const PORT = process.env.PORT || 5000
-const app = express();
 
-app.use(cors());app.use(express.json());
+require("./db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/blogs", blogRouter);
+
 app.use("/api", (req, res) => {
-  res.send("Hello World!");});
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => console.log(`app is running at ${PORT}`));
